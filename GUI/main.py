69b6eda5e59a9f70117c2bff23a1d228deaa9c48
Samuel Clay <samuel.clay99@gmail.com>
2021-09-25 15:37:23 -0600

    working TODO: fix touchscreen segfault

--- GUI/main.py
+++ GUI/main.py
@@ -1,79 +1,108 @@
 #!/usr/bin/env python3
-
+import mpv
+import locale
+import time
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gtk, Gdk, GdkPixbuf, GLib
 
 class TourGuideUI(Gtk.Window):
     def __init__(self):
+        self.open = True
         self.window = "none"
         super(TourGuideUI, self).__init__()
-
-    def init_ui(self):
-        #grid = Gtk.Grid()
-        #self.add(grid)
         self.fullscreen()
         self.set_keep_above(True)
         self.set_title("TourGuideUI")
-        self.set_size_request(250, 180)
-
-        self.set_border_width(30)
+        self.set_size_request(1920, 1080)
+        #self.set_border_width(30)
         self.connect("destroy", Gtk.main_quit)
 
-    def init_wait(self):
-        grid = Gtk.Grid()
+    def ready_wait(self):
+        # Set up the gif
+        image = Gtk.Image()
+        width = Gdk.Screen.get_default().get_width()
+        height = Gdk.Screen.get_default().get_height()
+        if (width == 3840 and height == 2160):
+            filename = "ready_wait_4k.gif"
+        elif (width == 1920 and height == 1080):
+            filename = "ready_wait_1080p.gif"
+        else:
+            filename = "ready_wait_1080p.gif"
+        wait_animation = GdkPixbuf.PixbufAnimation.new_from_file(filename)
+        image.set_from_animation(wait_animation)
+
+        # Set up button functionality
+        ready_btn = Gtk.Button()
+        ready_btn.add(image)
+        ready_btn.connect("clicked", Gtk.main_quit)
+        self.add(ready_btn)
+        self.show_all()
+
+    def intro(self):
+
+        button_width = 1000
+        button_height = 700
+
+        button1 = Gtk.Button(label="Room 1")
+        button1.set_size_request(button_width, button_height)
+        button2 = Gtk.Button(label="Room 2")
+        button2.set_size_request(button_width, button_height)
+        button3 = Gtk.Button(label="Room 3")
+        button3.set_size_request(button_width, button_height)
+        button4 = Gtk.Button(label="Room 4")
+        button4.set_size_request(button_width, button_height)
+        button5 = Gtk.Button(label="Room 5")
+        button5.set_size_request(button_width, button_height)
+        button6 = Gtk.Button(label="Room 6")
+        button6.set_size_request(button_width, button_height)
+
+        grid = Gtk.Grid();
+        grid.set_border_width(30)
+        grid.add(button1);
+        grid.attach(button2, 1, 0, 2, 1);
+        grid.attach_next_to(button3, button1, Gtk.PositionType.BOTTOM, 1, 2);
+        grid.attach_next_to(button4, button2, Gtk.PositionType.BOTTOM, 2, 1);
+        grid.attach(button5, 1, 2, 1, 1);
+        grid.attach_next_to(button6, button5, Gtk.PositionType.RIGHT, 1, 1);
         self.add(grid)
-        ok_btn = Gtk.Button(label="Ready")
-        ok_btn.set_size_request(80, 30)
-        ok_btn.connect("clicked", self.on_button_clicked)
-        grid.attach(ok_btn, 0, 0, 1, 1)
 
-    def room_select(self):
-        self.remove(grid)
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        quit_button = Gtk.Button(label="Quit")
+        quit_button.set_size_request(600, 600)
+        quit_button.connect("clicked", Gtk.main_quit)
+        grid.attach(quit_button, 3, 2, 1, 1)
 
-        stack = Gtk.Stack()
-        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
-        stack.set_transition_duration(1000)
-        
-        #checkbutton stack
-        checkbutton = Gtk.CheckButton(label="Click me!")
-        stack.add_titled(checkbutton, "check", "Check Button")
-        
-        #label stack
-        label = Gtk.Label()
-        label.set_markup("<big>A fancy label</big>")
-        stack.add_titled(label, "label", "A label")
+        video = Gtk.Frame()
+        grid.attach(video, 3, 1, 1, 1)
+        self.show_all()
+        self.mpv = mpv.MPV(wid=str(video.get_property("window").get_xid()))
+        self.mpv.play("intro.mkv")
+        self.mpv.terminate()
 
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-        vbox.pack_start(stack_switcher, True, True, 0)
-        vbox.pack_start(stack, True, True, 0)
-
-        #label = Gtk.Label("Where do you want to go?")
-        #grid.attach(label, 0, 0, 1, 1)
-    def on_button_clicked(self, widget):
-        self.room_select()
-        #Gtk.main_quit()
-
-# Main menu
-
-
-# Language selection
-
-
-# Room to go to
+    def quit(self):
+        Gtk.main_quit()
 
 
 # Main loop
 def main():
-    win = TourGuideUI()
-    win.init_ui()
-    win.init_wait()
-    win.show_all()
+    # Wait for user input (a touch)
+    locale.setlocale(locale.LC_NUMERIC, 'C')
+
+    ready_wait = TourGuideUI()
+    ready_wait.ready_wait()
     Gtk.main()
 
+    # play a welcome video
+    intro = TourGuideUI()
+    intro.intro()
+    Gtk.main()
+
+    # room selection
+    #room_select = TourGuideUI()
+    #room_select.room_select()
+    #room_select.show_all()
+    #Gtk.main()
 
 if __name__ == "__main__":
     main()

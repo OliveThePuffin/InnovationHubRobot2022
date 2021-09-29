a2981cc13859a761ba04a57ce84a80fb0d983555
Samuel Clay <samuel.clay99@gmail.com>
2021-09-29 16:40:38 -0600

    Update main.cpp

--- CppGUI/src/main.cpp
+++ CppGUI/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.guide.ui");
+	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.tour.guide");
 	TourGuideUI gui;
 
 	//shows the window and returns when it is closed.

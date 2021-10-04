19eeec2550b0fda77f734c6ad810efd19d4f710f
Samuel Clay <samuel.clay99@gmail.com>
2021-10-04 15:16:47 -0600

    Revert "trailing whitespace"
    This reverts commit 482f5c386c2ecb58f5767d2351eb5c6d2a05b159.
    
    	modified:   CppGUI/include/TourGuideUI.h
    	modified:   CppGUI/src/main.cpp

--- CppGUI/src/main.cpp
+++ CppGUI/src/main.cpp
@@ -1,7 +1,7 @@
 #include "TourGuideUI.h"
 #include <gtkmm/application.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.tour.guide");
 	TourGuideUI gui;

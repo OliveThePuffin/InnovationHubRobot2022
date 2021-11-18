e4b7a2195f8b741b4fda0d8c20cc7f795f45c32f
Sa1ntJul1an <94638350+Sa1ntJul1an@users.noreply.github.com>
2021-11-18 11:01:02 -0700

    Update main.py

--- compvis/main.py
+++ compvis/main.py
@@ -1,1 +1,134 @@
+# TEST PEDESTRIAN TRACKING
 
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+
+
+def main():
+    video_feed = cv2.VideoCapture(0)
+    width = int(video_feed.get(3))
+    height = int(video_feed.get(4))
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    videoWriter = cv2.VideoWriter("output.avi", fourcc=fourcc, fps=30.0, frameSize=(width, height))
+
+    pipeline = rs.pipeline()
+
+    # Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+
+    # Get device product line for setting a supporting resolution
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
+    device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("The demo requires Depth camera with Color sensor")
+        exit(0)
+
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+    if device_product_line == 'L500':
+        config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+    else:
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    profile = pipeline.start(config)
+
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+
+    # We will be removing the background of objects more than clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 3  # meters
+    clipping_distance = clipping_distance_in_meters / depth_scale
+
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
+
+    # Streaming loop
+    try:
+        while True:
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
+
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
+
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
+
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
+
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # Remove background - Set pixels further than clipping_distance to grey
+            grey_color = 0
+            depth_image_3d = np.dstack(
+                (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+            bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+            # Render images:
+            #   depth align to color on left
+            #   depth on right
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+            # PEDESTRIAN TRACKING
+            # ==========================================================================================================
+            hog = cv2.HOGDescriptor()
+            hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+            boxes, weights = hog.detectMultiScale(bg_removed, winStride=(8, 8))
+            npboxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+            for (xA, yA, xB, yB) in npboxes:
+                print(f"Person Detected: ({round((xA + xB) / 2)}, {round((yA + yB) / 2)})")
+                cv2.rectangle(bg_removed, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+            # cv2.imshow("Test", color_image)
+            # ==========================================================================================================
+
+            # write to output
+            # videoWriter.write(frame)
+
+            depth_colormap_dim = depth_colormap.shape
+            color_colormap_dim = bg_removed.shape
+
+            if depth_colormap_dim != color_colormap_dim:
+                resized_color_image = cv2.resize(bg_removed, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
+                                                 interpolation=cv2.INTER_AREA)
+                images = np.hstack((resized_color_image, depth_colormap))
+
+            else:
+                images = np.hstack((bg_removed, depth_colormap))
+
+            cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('Align Example', images)
+            key = cv2.waitKey(1)
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+    finally:
+        pipeline.stop()
+
+    # videoWriter.release()
+
+
+if __name__ == "__main__":
+    main()

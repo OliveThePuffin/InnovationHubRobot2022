37cdc17b25157d49e1377cb141766bffcdb6b6d5
Sa1ntJul1an <jtrman73739@gmail.com>
2022-01-20 10:43:08 -0700

    edited main.py

--- compvis/main.py
+++ compvis/main.py
@@ -1,21 +1,36 @@
-# TEST PEDESTRIAN TRACKING
+# OBJECT DETECTION
 
 import cv2
 import numpy as np
 import pyrealsense2 as rs
+from imutils.object_detection import non_max_suppression
+
+ROBOT_WIDTH = 0.6  # meters
+ROBOT_HEIGHT = 1  # meters
+SAFE_DISTANCE = 1.5  # meters
+CLIPPING_DISTANCE = 3  # meters
 
 
 def main():
     video_feed = cv2.VideoCapture(0)
-    width = int(video_feed.get(3))
-    height = int(video_feed.get(4))
+    img_width = int(video_feed.get(3))
+    img_height = int(video_feed.get(4))
     fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    videoWriter = cv2.VideoWriter("output.avi", fourcc=fourcc, fps=30.0, frameSize=(width, height))
+    videoWriter = cv2.VideoWriter("output.avi", fourcc=fourcc, fps=30.0, frameSize=(img_width, img_height))
 
     pipeline = rs.pipeline()
 
+    # camera properties:
+    profile = pipeline.get_active_profile()
+    depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+    intrinsics = depth_profile.get_intrinsics()
+    cx = intrinsics.ppx
+    cy = intrinsics.ppy
+    fx = intrinsics.fx
+    fy = intrinsics.fy
+
     # Create a config and configure the pipeline to stream
-    #  different resolutions of color and depth streams
+    # different resolutions of color and depth streams
     config = rs.config()
 
     # Get device product line for setting a supporting resolution
@@ -48,8 +63,7 @@ def main():
     depth_scale = depth_sensor.get_depth_scale()
 
     # We will be removing the background of objects more than clipping_distance_in_meters meters away
-    clipping_distance_in_meters = 3  # meters
-    clipping_distance = clipping_distance_in_meters / depth_scale
+    clipping_distance = CLIPPING_DISTANCE / depth_scale
 
     # Create an align object
     # rs.align allows us to perform alignment of depth frames to others frames
@@ -83,25 +97,100 @@ def main():
             depth_image_3d = np.dstack(
                 (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
             bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+            grayscale = cv2.cvtColor(bg_removed, cv2.COLOR_BGR2GRAY)
+            _, binary_img = cv2.threshold(grayscale, thresh=1, maxval=255, type=cv2.THRESH_BINARY)
+
+            # create box filter kernels for closing and opening
+            kernel_close = np.ones((10, 10), np.uint8)
+            kernel_open = np.ones((5, 5), np.uint8)
+
+            # closing
+            binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel_close)
+            # opening
+            binary_img = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel_open)
+
+            cv2.imshow("Binary Image", binary_img)
+
+            # get connected components
+            nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(binary_img, connectivity=8)
+            if centroids is not None:  # if at least one object is found
+
+                # PEDESTRIAN TRACKING WITH HOG FILTER
+                # ======================================================================================================
+                hog = cv2.HOGDescriptor()
+                hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+                boxes, weights = hog.detectMultiScale(color_image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+                npboxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+                pick = non_max_suppression(npboxes, probs=None, overlapThresh=0.65)
+                for (xA, yA, xB, yB) in pick:
+                    person_center = (((xA + xB) / 2), ((yA + yB) / 2))
+                    depth_center = aligned_depth_frame.get_distance(round(person_center[0]), round(person_center[1]))
+                    if depth_center < CLIPPING_DISTANCE:
+                        print(f"Person Detected: ({round((xA + xB) / 2)}, {round((yA + yB) / 2)})")
+                        cv2.rectangle(bg_removed, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+                        person_bounding_box = get_bounding_box_meters(depth_center, xA, yA, xB, yB, fx, fy, cx, cy)
+                        person_center_meters = person_bounding_box[0]
+                        person_bottom_left_meters = person_bounding_box[3]
+                        person_bottom_right_meters = person_bounding_box[4]
+
+                        if check_for_obstruction(person_center_meters[0], person_center_meters[1], depth_center):
+                            person_in_path = True
+                        elif check_for_obstruction(person_bottom_left_meters[0], person_bottom_left_meters[1],
+                                                   depth_center):
+                            person_in_path = True
+                        elif check_for_obstruction(person_bottom_right_meters[0], person_bottom_right_meters[1],
+                                                   depth_center):
+                            person_in_path = True
+                        else:
+                            person_in_path = False
+                # ======================================================================================================
+
+                for detected_object in range(nb_components):
+
+                    # draw bounding  boxes:
+                    left_edge = stats[detected_object, cv2.CC_STAT_LEFT]
+                    top_edge = stats[detected_object, cv2.CC_STAT_TOP]
+                    box_height = stats[detected_object, cv2.CC_STAT_HEIGHT]
+                    box_width = stats[detected_object, cv2.CC_STAT_WIDTH]
+
+                    cv2.rectangle(bg_removed, color=(0, 0, 255), pt1=(left_edge, top_edge),
+                                  pt2=(left_edge + box_width, top_edge + box_height))
+
+                    center = centroids[detected_object]
+
+                    x = round(center[0])
+                    y = round(center[1])
+                    distance = aligned_depth_frame.get_distance(x, y)
+
+                    if distance < CLIPPING_DISTANCE:
+
+                        # transform of camera coordinates to world coordinates
+                        x_w = ((x - cx) * distance)/fx
+                        y_w = -((y - cy) * distance)/fy
+
+                        path_obstructed = False
+                        if (x_w > ROBOT_WIDTH/2 or x_w < -ROBOT_WIDTH/2) or y_w > ROBOT_HEIGHT \
+                                or distance > SAFE_DISTANCE:  # safe area
+                            marker_color = (0, 255, 0)
+                        else:
+                            path_obstructed = True
+                            marker_color = (0, 0, 255)
+                        cv2.drawMarker(bg_removed, markerType=cv2.MARKER_CROSS, markerSize=40, thickness=2,
+                                       color=marker_color,
+                                       position=(x, y))
+                        cv2.putText(bg_removed, fontScale=2, fontFace=cv2.FONT_HERSHEY_PLAIN,
+                                    org=(x, y), color=marker_color, thickness=2,
+                                    text=f"({round(x_w, 2)}, {round(y_w, 2)}, {round(distance, 2)})")
+
+                        if path_obstructed:
+                            print(f"Object Detected, Centered at: ({x_w}, {y_w}, {distance}) meters")
 
             # Render images:
             #   depth align to color on left
             #   depth on right
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-            # PEDESTRIAN TRACKING
-            # ==========================================================================================================
-            hog = cv2.HOGDescriptor()
-            hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-            boxes, weights = hog.detectMultiScale(bg_removed, winStride=(8, 8))
-            npboxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-            for (xA, yA, xB, yB) in npboxes:
-                print(f"Person Detected: ({round((xA + xB) / 2)}, {round((yA + yB) / 2)})")
-                cv2.rectangle(bg_removed, (xA, yA), (xB, yB), (0, 255, 0), 2)
-
-            # cv2.imshow("Test", color_image)
-            # ==========================================================================================================
 
             # write to output
             # videoWriter.write(frame)
@@ -132,3 +221,42 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+def get_bounding_box_meters(depth_at_center, top_left_x, center_x, center_y, top_left_y, bottom_right_x, bottom_right_y,
+                            fx, fy, cx, cy):
+    # transform of camera coordinates to world coordinates
+    # CENTER COORDINATE IN METERS
+    x_w_center = ((center_x - cx) * depth_at_center) / fx
+    y_w_center = -((center_y - cy) * depth_at_center) / fy
+
+    # TOP LEFT COORDINATE IN METERS
+    x_w_top_left = ((top_left_x - cx) * depth_at_center) / fx
+    y_w_top_left = -((top_left_y - cy) * depth_at_center) / fy
+
+    # BOTTOM RIGHT
+    x_w_bottom_right = ((bottom_right_x - cx) * depth_at_center) / fx
+    y_w_bottom_right = -((bottom_right_y - cy) * depth_at_center) / fy
+
+    # TOP RIGHT
+    x_w_top_right = x_w_bottom_right
+    y_w_top_right = y_w_top_left
+
+    # BOTTOM LEFT
+    x_w_bottom_left = x_w_top_left
+    y_w_bottom_left = y_w_bottom_right
+
+    bounding_box_world = np.array([[x_w_center, y_w_center],
+                                   [x_w_top_left, y_w_top_left],
+                                   [x_w_top_right, y_w_top_right],
+                                   [x_w_bottom_left, y_w_bottom_left],
+                                   [x_w_bottom_right, y_w_bottom_right]])
+
+    return bounding_box_world
+
+
+def check_for_obstruction(x, y, z):
+    if (x > ROBOT_WIDTH / 2 or x < -ROBOT_WIDTH / 2) or y > ROBOT_HEIGHT or z > SAFE_DISTANCE:  # safe area
+        return False
+    else:
+        return True

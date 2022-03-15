9d931cf4bd97bebf81c68f24a9df62452337afc7
UncleDrew92 <akdavis@mines.edu>
2022-03-15 09:56:24 -0600

    Renamed ObjectDetection file, and updated FuyzzyLogicSimulation code before sprint review demo

--- compvis/FuzzyLogicSimulation.py
+++ compvis/FuzzyLogicSimulation.py
@@ -4,12 +4,14 @@ from _testcapi import INT_MAX
 
 import skfuzzy
 import numpy as np
+from matplotlib.collections import LineCollection
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-class Rectangle:
 
+class Rectangle:
     """ A class of Python object that describes the properties of a rectangle"""
+
     def __init__(self, x1, x2, y1, y2):
         self.angle = None
         self.y_min_distance = None
@@ -27,12 +29,7 @@ class Rectangle:
         y_center = (self.y1 + self.y2) / 2
         x_min_distance = abs(x_center - x_loc)
         y_min_distance = abs(y_center - y_loc)
-        # x1_distance = self.x1 - x_loc
-        # x2_distance = self.x2 - x_loc
-        # x_min_distance = min(abs(x1_distance), abs(x2_distance))
-        # y1_distance = self.y1 - y_loc
-        # y2_distance = self.y2 - y_loc
-        # y_min_distance = min(abs(y1_distance), abs(y2_distance))
+
         self.x_min_distance = x_min_distance
         self.y_min_distance = y_min_distance
 
@@ -40,7 +37,7 @@ class Rectangle:
         return math.sqrt(math.pow(self.x_min_distance, 2) + math.pow(self.y_min_distance, 2))
 
     def compute_angle(self, robot_heading):
-        self.angle = (90 + math.atan(self.x_min_distance / self.y_min_distance) * 180/math.pi) + (robot_heading - 90)
+        self.angle = (90 + math.atan(self.x_min_distance / self.y_min_distance) * 180 / math.pi) + (robot_heading - 90)
 
 
 class Robot:
@@ -62,46 +59,54 @@ class Robot:
         return self.heading
 
     def calculate_distance_moved(self, speed):
-        self.distance = speed / 10000
+        self.distance = speed / 20000
 
     def update_xy(self):
-        self.delta_y_error = -self.distance * math.cos((self.heading - 90) * math.pi/180)
-        self.x = self.x + self.distance * math.sin((self.heading - 90) * math.pi/180)
-        self.y = self.y + self.distance * math.cos((self.heading - 90) * math.pi/180)
+        self.delta_y_error = -self.distance * math.cos((self.heading - 90) * math.pi / 180)
+        self.x = self.x + self.distance * math.sin((self.heading - 90) * math.pi / 180)
+        self.y = self.y + self.distance * math.cos((self.heading - 90) * math.pi / 180)
 
         self.x_error = self.x - self.x_destination
-        # if self.x_error > 2:
-        #     self.x_error = 2
+        if self.x_error > 4:
+            self.x_error = 3.8
+
+        if self.x_error < -4:
+            self.x_error = -3.8
 
         self.y_error = self.y_destination - self.y
 
+
 def main():
-    distance = ctrl.Antecedent(np.arange(0, 3.2, 0.08), 'distance')  # meters
+    # distance = ctrl.Antecedent(np.arange(0, 3.2, 0.08), 'distance')  # meters
+    distance = ctrl.Antecedent(np.arange(0, 1, 0.1), 'distance')  # meters
     angle = ctrl.Antecedent(np.arange(0, 181, 1), 'angle')  # meters
     x_error = ctrl.Antecedent(np.arange(-4, 4.1, 0.1), 'x_error')  # meters
-    delta_y_error = ctrl.Antecedent(np.arange(-2, 2.08, 0.08), "delta_y_error")  # meters
+    delta_y_error = ctrl.Antecedent(np.arange(-.2, .2, 0.01), "delta_y_error")  # meters
 
     speed = ctrl.Consequent(np.arange(0, 401, 1), 'speed')  # motor input
     heading = ctrl.Consequent(np.arange(0, 181, 1), 'heading')  # degrees
 
     # ANTECEDENTS
-    distance['zero'] = skfuzzy.trimf(distance.universe, [0, 0.25, 0.5])
-    distance['low'] = skfuzzy.trimf(distance.universe, [.25, 1.5, 2.08])
-    distance['high'] = skfuzzy.trimf(distance.universe, [1.8, 2.5, 3.12])
+    # distance['zero'] = skfuzzy.trimf(distance.universe, [0, 0.25, 0.5])
+    # distance['low'] = skfuzzy.trimf(distance.universe, [.25, 1.5, 2.08])
+    # distance['high'] = skfuzzy.trimf(distance.universe, [1.8, 2.5, 3.12])
+    distance['zero'] = skfuzzy.trimf(distance.universe, [0, 0.15, 0.3])
+    distance['low'] = skfuzzy.trimf(distance.universe, [0.3, 0.5, 0.7])
+    distance['high'] = skfuzzy.trimf(distance.universe, [0.6, 0.8, 1])
 
-    angle['left'] = skfuzzy.trimf(angle.universe, [0, 0, 45])
+    angle['left'] = skfuzzy.trimf(angle.universe, [-30, -30, 45])
     angle['mid_left'] = skfuzzy.trimf(angle.universe, [0, 45, 90])
     angle['center'] = skfuzzy.trimf(angle.universe, [75, 90, 105])
     angle['mid_right'] = skfuzzy.trimf(angle.universe, [90, 135, 180])
-    angle['right'] = skfuzzy.trimf(angle.universe, [135, 180, 180])
+    angle['right'] = skfuzzy.trimf(angle.universe, [135, 210, 210])
 
-    x_error['negative'] = skfuzzy.trimf(x_error.universe, [-4, -2, 0])
+    x_error['negative'] = skfuzzy.trimf(x_error.universe, [-4, -4, 0])
     x_error['zero'] = skfuzzy.trimf(x_error.universe, [-1, 0, 1])
-    x_error['positive'] = skfuzzy.trimf(x_error.universe, [0, 2, 4])
+    x_error['positive'] = skfuzzy.trimf(x_error.universe, [0, 4, 4])
 
-    delta_y_error['negative'] = skfuzzy.trimf(delta_y_error.universe, [-2, -2, 0])
-    delta_y_error['zero'] = skfuzzy.trimf(delta_y_error.universe, [-0.5, 0, 0.5])
-    delta_y_error['positive'] = skfuzzy.trimf(delta_y_error.universe, [0, 2, 2])
+    delta_y_error['negative'] = skfuzzy.trimf(delta_y_error.universe, [-.2, -.2, 0])
+    delta_y_error['zero'] = skfuzzy.trimf(delta_y_error.universe, [-0.05, 0, 0.05])
+    delta_y_error['positive'] = skfuzzy.trimf(delta_y_error.universe, [0, .2, .2])
 
     # CONSEQUENTS
     speed['zero'] = skfuzzy.trimf(speed.universe, [0, 0, 50])
@@ -139,24 +144,55 @@ def main():
 
     speed_control = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10,
                                         rule11, rule12, rule13, rule14, rule15])
+
     speed_sim = ctrl.ControlSystemSimulation(speed_control)
+
+    """ TESTING SINGLE FUZZY LOGIC OUTPUTS """
+    # speed_sim.input['x_error'] = 0
+    # speed_sim.input['distance'] = .9
+    # speed_sim.input['delta_y_error'] = .19
+    # speed_sim.input['angle'] = 179
+    #
+    # speed_sim.compute()
+    #
+    # print("Speed: " + str(speed_sim.output['speed']))
+    # print("Heading: " + str(speed_sim.output['heading']))
+    #
+    # # CONSEQUENTS:
+    # # speed.view(sim=speed_sim)
+    # # heading.view(sim=speed_sim)
+    # delta_y_error.view(sim=speed_sim)
+    #
+    # # ANTECEDENTS:
+    # # distance.view(sim=speed_sim)
+    # # angle.view(sim=speed_sim)
+    #
+    # print("Speed: ")
+    # print(speed_sim.output['speed'])
+    # print("Heading: ")
+    # print(speed_sim.output['heading'])
 
     # Object locations at
     objects = []
-    for i in range(10):
-        rand1 = random.randint(5, 8)
-        rand2 = random.randint(0, 10)
+    for i in range(100):
+        rand1 = random.random() * 10
+        # rand1 = random.random() * ((8 - 5) + 1) + 3
+        rand2 = random.random() * 10
+        while rand1 == 6 and rand2 == 0:
+            rand1 = random.random() * 10
+            # rand1 = random.random() * ((8 - 5) + 1) + 3
+            rand2 = random.random() * 10
         objects.append(Rectangle(rand1, rand1 + .03, rand2, rand2 + .3))
-    # rectangle1 = Rectangle(6, 6.05, 3, 3.3)
+    # rectangle1 = Rectangle(5.65, 5.7, 2, 2.3)
     # rectangle2 = Rectangle(6.45, 6.50, 7, 7.3)
-    # rectangle2 = Rectangle(6.15, 6.20, 6, 6.3)
-    # rectangle3 = Rectangle(6.45, 6.50, 7, 7.3)
+    # rectangle3 = Rectangle(6.15, 6.20, 6, 6.3)
+    # rectangle4 = Rectangle(6.45, 6.50, 7, 7.3)
     #
-    # objects = [rectangle1, rectangle2, rectangle3]
+    # objects = [rectangle1]
     # objects = [rectangle1, rectangle2]
 
     # Creates robot start location and destination locations
-    robot = Robot(6, 0, 6.5, 15)
+    robot = Robot(0, 0, 10, 15)
 
     # Calculate the min distance to one of the 4 corners
     min_xy_distance = INT_MAX
@@ -175,12 +211,14 @@ def main():
     objects[index].compute_angle(robot.get_heading())
     x_path = []
     y_path = []
+    speed_at_loc = []
 
     x_path.append(robot.x)
     y_path.append(robot.y)
+    speed_at_loc.append(0)
     robot.update_xy()
 
-    while abs(robot.x_error) > .2 or abs(robot.y_error) > .2:
+    while abs(robot.x_error) > .5 or abs(robot.y_error) > .2:
         speed_sim.input['x_error'] = robot.x_error
         speed_sim.input['distance'] = min_xy_distance
         speed_sim.input['delta_y_error'] = robot.delta_y_error
@@ -198,6 +236,9 @@ def main():
 
         x_path.append(robot.x)
         y_path.append(robot.y)
+        print("X: " + str(robot.x))
+        print("Y: " + str(robot.y))
+        speed_at_loc.append(new_speed)
 
         min_xy_distance = INT_MAX
         index = 0
@@ -214,7 +255,7 @@ def main():
         objects[index].compute_angle(robot.get_heading())
 
     fig, ax = plt.subplots()
-    plt.xlim([4, 9])
+    plt.xlim([0, 12])
     plt.ylim([0, 17])
     ax.plot(x_path, y_path)
 
@@ -227,6 +268,18 @@ def main():
         ax.add_patch(plt.Rectangle((loc_list[i][0], loc_list[i][2]), width, height, fc='r'))
 
     ax.add_patch(plt.Circle((robot.x_destination, robot.y_destination), .2, fc='g'))
+
+    # Create a continuous norm to map from data points to colors
+    norm = plt.Normalize(min(speed_at_loc), max(speed_at_loc))
+    points = np.array([x_path, y_path]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(speed_at_loc)
+    lc.set_linewidth(2)
+    line = ax.add_collection(lc)
+    fig.colorbar(line, ax=ax)
+
     plt.show()
 
     # # CONSEQUENTS:
@@ -242,6 +295,7 @@ def main():
     # print(speed_sim.output['speed'])
     # print("Heading: ")
     # print(speed_sim.output['heading'])
+
 
 if __name__ == '__main__':
     main()

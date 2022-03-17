38599d28f5a3c679841cd31f3bd44884630dedf1
UncleDrew92 <akdavis@mines.edu>
2022-03-17 09:58:55 -0600

    Changed SerialTest to show a string

--- compvis/SerialTest.py
+++ compvis/SerialTest.py
@@ -1,19 +1,10 @@
 import serial
-import random
 
 
 def main():
-    x = random.randint(0, 10)
-    y = random.randint(0, 10)
     ser = serial.Serial('/dev/ttyACM0')
-
-    for i in range(100):
-        ser.write(f'{x}'.encode("utf-8"))
-        ser.write("\n")
-        ser.write(f'{y}'.encode("utf-8"))
-
-        x = random.randint(0, 10)
-        y = random.randint(0, 10)
+    message = "Serial Test"
+    ser.write(bytes(message, 'utf-8'))
 
     ser.close()
 

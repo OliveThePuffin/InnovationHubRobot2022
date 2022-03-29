92646083ce884428574504870dbdb8c36671bb9e
UncleDrew92 <akdavis@mines.edu>
2022-03-29 09:04:31 -0600

    Changed Serial Test to constantly run

--- compvis/SerialTest.py
+++ compvis/SerialTest.py
@@ -4,7 +4,8 @@ import serial
 def main():
     ser = serial.Serial('/dev/ttyACM0', baudrate=9600)
     message = "Serial Test"
-    ser.write(bytes(message, 'utf-8'))
+    while True:
+        ser.write(bytes(message, 'utf-8'))
 
     ser.close()
 

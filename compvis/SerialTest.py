6563f2c6caeff52e07064400cd10724c740b1a4b
UncleDrew92 <akdavis@mines.edu>
2022-03-29 08:58:54 -0600

    Changed baudrate in SerialTest

--- compvis/SerialTest.py
+++ compvis/SerialTest.py
@@ -2,7 +2,7 @@ import serial
 
 
 def main():
-    ser = serial.Serial('/dev/ttyACM0')
+    ser = serial.Serial('/dev/ttyACM0', baudrate=9600)
     message = "Serial Test"
     ser.write(bytes(message, 'utf-8'))
 

16d8c003a63c502ca177fce2e25a6432436b8051
UncleDrew92 <akdavis@mines.edu>
2022-04-21 09:54:12 -0600

    added marvelmind python file

---
+++ GUI/README.md
@@ -0,0 +1,19 @@
+# Tour Guide GUI
+
+This project requires the `gtkmm3` and `mpv` packages to be installed.
+
+Because this GUI uses the GTK API, it can be customized with different GTK themes
+
+https://www.gnome-look.org/browse?cat=135
+https://www.gnome-look.org/p/1271140
+https://www.gnome-look.org/p/1441725
+
+
+Currently the binary must be run in this dir as `bin/TGUI` or the media files won't load
+
+TODO:
+	Make binary runable with media files anywhere
+	fix pixel formatting on ready.gif
+	fix alignment of GUI on tablet
+
+Feedback: Make room selection layout show current location
---
+++ GUI/include/TourGuideUI.h
@@ -0,0 +1,81 @@
+//#include "TourGuideUI.h"
+
+#ifndef GTKMM_TOUR_GUIDE_UI_H
+#define GTKMM_TOUR_GUIDE_UI_H
+
+#define GENERAL_BORDER_WIDTH	s_height/48
+#define MENU_BORDER_WIDTH	s_height/32
+
+#define ROOM_BUTTON_WIDTH	(s_width - (2*GENERAL_BORDER_WIDTH)) / 9
+#define ROOM_BUTTON_HEIGHT	(s_height - (2*GENERAL_BORDER_WIDTH)) / 2
+
+//execute system shell commands
+//string support
+#include <stdlib.h>
+#include <string>
+
+//GTK widgets to import
+//GDK Pixbuf (to show animations)
+#include <gtkmm.h>
+#include <gdkmm/pixbufanimation.h>
+
+class TourGuideUI : public Gtk::Window
+{
+
+	public:
+		TourGuideUI();
+		virtual ~TourGuideUI();
+
+		//Hard coded screen resolution
+		const int s_width  =	800;
+		const int s_height =	480;
+
+	private:
+		//Helper functions
+		void setup_stack();
+		void setup_ready();
+		void setup_main_menu();
+		void setup_room_select();
+		void setup_options();
+
+		//Stack
+		Gtk::Stack	m_stack;
+
+		//Ready
+		Gtk::Button	ready_button;
+		Gtk::Image	ready_gif;
+
+		//Main Menu
+		Gtk::VBox	main_menu;
+		Gtk::Button	main_tour;
+		Gtk::Button	main_select;
+		Gtk::Button	main_options;
+		Gtk::Button	main_quit;
+
+		//Room Select
+		Gtk::Grid	room_grid;
+		Gtk::Button	room_capstone;
+		Gtk::Button	room_composites;
+		Gtk::Button	room_electronics;
+		Gtk::Button	room_cornerstone;
+		Gtk::Button	room_cafe;
+		Gtk::Button	room_metals;
+		Gtk::Button	room_wood;
+		Gtk::Button	room_back_button;
+
+		//Options
+		Gtk::Grid	options_grid;
+		Gtk::Button	options_back_button;
+
+		//Media files
+		std::string video_player = "mpv --fs ";
+		std::string ready_file = "media/ready.gif";
+		std::string intro_file = "media/intro.mkv";
+
+		//Signal Handlers:
+		void goto_menu(bool intro);
+		void goto_rooms();
+		void goto_options();
+};
+
+#endif
